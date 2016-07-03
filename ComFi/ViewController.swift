d33290d6b390de72de9200fffba2f25e5feb0a32
zzzel <marko@zzzel.xyz>
2016-07-03 05:31:27 +0200

    Initial Commit

---
+++ ComFi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ComFi
+//
+//  Created by Zel Marko on 7/3/16.
+//  Copyright © 2016 Zel Marko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
